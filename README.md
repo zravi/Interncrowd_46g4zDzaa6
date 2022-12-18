1cba84729e055052fc4d73facb258873a35a4f81
RΛVI <12195025+zravi@users.noreply.github.com>
2022-12-18 13:33:41 +0530

    font

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Interncrowd_46g4zDzaa6
 
 used multiple css made landing page for interncrowd !!
+fonts library used for better fonts like themefiy and googlefonts!!
--- landing_page/js/script.js
+++ landing_page/js/script.js
@@ -4,7 +4,7 @@ Style   : script.js
 
 */
 
-$(function() {
+$(function () {
     "use strict";
 
     /*-----------------------------------
@@ -19,24 +19,24 @@ $(function() {
         }
     }
     menuscroll();
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         menuscroll();
     });
     /*-----------------------------------
      * NAVBAR CLOSE ON CLICK
      *-----------------------------------*/
 
-    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function () {
         $('.navbar-collapse').collapse('hide');
     });
     /* 
      * NAVBAR TOGGLE BG
      *-----------------*/
     var siteNav = $('#navbar');
-    siteNav.on('show.bs.collapse', function(e) {
+    siteNav.on('show.bs.collapse', function (e) {
         $(this).parents('.nav-menu').addClass('menu-is-open');
     })
-    siteNav.on('hide.bs.collapse', function(e) {
+    siteNav.on('hide.bs.collapse', function (e) {
         $(this).parents('.nav-menu').removeClass('menu-is-open');
     })
 
@@ -44,7 +44,7 @@ $(function() {
      * ONE PAGE SCROLLING
      *-----------------------------------*/
     // Select all links with hashes
-    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function (event) {
         // On-page links
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             // Figure out element to scroll to
@@ -56,7 +56,7 @@ $(function() {
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000, function() {
+                }, 1000, function () {
                     // Callback after animation
                     // Must change focus!
                     var $target = $(target);
